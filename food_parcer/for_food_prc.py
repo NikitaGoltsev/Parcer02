769eaca379c3a7d166fdccad9ef96e434d4498b8
FedorsHome1 <nik.goltsev.97@bk.ru>
2023-04-08 23:09:15 +0700

    With_saved_page

--- food_parcer/for_food_prc.py
+++ food_parcer/for_food_prc.py
@@ -8,8 +8,17 @@ import requests
 url = "https://health-diet.ru/table_calorie/?utm_source=leftMenu&utm_medium=table_calorie"
 
 # Work with  url and requests lib
-headers = { # That's to update
-    
+headers = { # That's to update user agents, to show web page - we are not bots
+    "Accept" : "*/*",
+    "User-Agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"
+    # That's parametrs to get acces for cite information
+    # We are able to check them into web page
+    # Also there are some libs to generate them ?
 }
 
-req = '' # the results of the requsts work
+req = requests.get(url, headers=headers)
+src = req.text
+#print(req)
+
+with open("index.html", "w") as file:
+    file.write(src) # To not be banned with to much acces from one device 
