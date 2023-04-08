9dba789a397e740b8dc9acc25d9497a1f634f94f
FedorsHome1 <nik.goltsev.97@bk.ru>
2023-04-08 21:55:40 +0700

    Correst_class_out

--- some_try/classes_finder.py
+++ some_try/classes_finder.py
@@ -1,6 +1,13 @@
 from bs4 import BeautifulSoup
 
-with open("html_file/index.html") as file:
-    src = file.read()
+#soup define some bs4 object
+#soup = ...
 
-print(src)
+with open("/home/helgenro/NSU_subjects/WEB_and_something/Parcer02/some_try/html_file/index.html") as file:
+    src = file.read()
+soup = BeautifulSoup(src, "lxml")
+# print(src) # the src is a bs4 object
+
+# class finder
+user_name = soup.find("div", class_="user__name")
+print(user_name)
