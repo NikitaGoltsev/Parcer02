aa21e5cf538a62ae55d52e982d8b9ac32b07d4d1
FedorsHome1 <nik.goltsev.97@bk.ru>
2023-04-08 22:53:23 +0700

    work_with_requests

--- food_parcer/for_food_prc.py
+++ food_parcer/for_food_prc.py
@@ -1,2 +1,15 @@
 from bs4 import BeautifulSoup
+import requests
+# despription - we need to get all product links and after
+# that we will be able to get there "состав?"
 
+#the link - "https://health-diet.ru/table_calorie/?utm_source=leftMenu&utm_medium=table_calorie"
+
+url = "https://health-diet.ru/table_calorie/?utm_source=leftMenu&utm_medium=table_calorie"
+
+# Work with  url and requests lib
+headers = { # That's to update
+    
+}
+
+req = '' # the results of the requsts work
