dc60052bb444c1ac1e37a5564b550eb39f23aaac
FedorsHome1 <nik.goltsev.97@bk.ru>
2023-04-12 22:16:55 +0700

    And_of_food_project

--- food_parcer/mai_part.py
+++ food_parcer/mai_part.py
@@ -2,6 +2,8 @@ from bs4 import BeautifulSoup
 import requests
 import json # to save something in json file
 
+from for_food_prc import headers
+
 with open("/home/helgenro/NSU_subjects/WEB_and_something/Parcer02/food_parcer/saved_web_page/index.html") as file:
     src = file.read()
 
@@ -16,29 +18,39 @@ all_categorise_dict = {}
 for item in all_prod_hrefs:
 
     item_text = item.text
-    item_href = item.get("href")
-    print(f"for {item_text} href is {item_href}")
+    item_href = "https://health-diet.ru" + item.get("href")
+    #print(f"for {item_text} href is {item_href}")
 
     all_categorise_dict[item_text] = item_href
 
 # Save to json file
-with open("all_cat.json", "w") as file:
-    json.dump(all_categorise_dict, file, indent=4, ensure_ascii=False)
-
+#with open("all_cat.json", "w") as file:
+    #json.dump(all_categorise_dict, file, indent=4, ensure_ascii=False)
+with open("all_cat.json") as file:
+    all_categorise = json.load(file)
 
 # 2) We need to craete cicle for everuone page
 #
 count = 0
-for cat_name, cat_href in all_categorise_dict:
+for cat_name, cat_href in all_categorise.items():
     
-    rep =[",", " ", "-", "'"]
-    for item in rep:
-        if item in cat_name:
-            cat_name = cat_name.replace(item, "_")
+    if count == 0: # That's to work only with one category for the first time
+        rep =[",", " ", "-", "'"]
+        for item in rep:
+            if item in cat_name:
+                cat_name = cat_name.replace(item, "_")
 
-    req = requests.get(url=cat_href, headers=headers)
-    src = req.text
-    with open() as file:
-        file.write(src)
+        #req = requests.get(url=cat_href)
+        #src = req.text
 
-    count+=
+        #with open("write_cd.html", "w") as file:
+            #file.write(src)
+
+        with open("write_cd.html") as file:
+            src = file.read()
+
+        soup = BeautifulSoup(src, "lxml")
+        #Let's get headers of the table
+        tabl_head = soup.find(class_="mzr-tc-group-table").find("tr").find_all("th")
+        #print(tabl_head)
+    count += 1
