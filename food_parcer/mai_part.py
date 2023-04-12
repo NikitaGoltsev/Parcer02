3707341a968d7dc38bfcb3bd81657688b7bbc64f
FedorsHome1 <nik.goltsev.97@bk.ru>
2023-04-12 20:23:53 +0700

    Work_with_everyone_page

--- food_parcer/mai_part.py
+++ food_parcer/mai_part.py
@@ -2,28 +2,43 @@ from bs4 import BeautifulSoup
 import requests
 import json # to save something in json file
 
-
-with open('/home/helgenro/NSU_subjects/WEB_and_something/Parcer02/food_parcer/saved_web_page/index.html') as file:
-
+with open("/home/helgenro/NSU_subjects/WEB_and_something/Parcer02/food_parcer/saved_web_page/index.html") as file:
     src = file.read()
 
-soup = BeautifulSoup(src, "lxml")
+soup = BeautifulSoup(src, 'lxml')
 
-all_links_dict = {}
+#1) We need to get all href from div
+# class of the hrefs is ""
 
-all_prod_hrefs = soup.find_all(class_= "mzr-tc-group-item-href")
+all_prod_hrefs = soup.find_all(class_ = "mzr-tc-group-item-href")
+
+all_categorise_dict = {}
 for item in all_prod_hrefs:
-    #print(item.get("href"))
-    item_name = item.text
-    item_href = "https://health-diet.ru" + item.get("href")
-    #             \-- add domaine name to find results
 
-    all_links_dict[item_name] = item_href # to save in dictonary
+    item_text = item.text
+    item_href = item.get("href")
+    print(f"for {item_text} href is {item_href}")
 
-for ct_name, ct_href in all_links_dict.items():
-    rep = [',', ' ', '-', "'"]
+    all_categorise_dict[item_text] = item_href
+
+# Save to json file
+with open("all_cat.json", "w") as file:
+    json.dump(all_categorise_dict, file, indent=4, ensure_ascii=False)
+
+
+# 2) We need to craete cicle for everuone page
+#
+count = 0
+for cat_name, cat_href in all_categorise_dict:
+    
+    rep =[",", " ", "-", "'"]
     for item in rep:
-        if item in ct_name:
-            ct_name = ct_name.replace(item, "_")
-    print(ct_name)
+        if item in cat_name:
+            cat_name = cat_name.replace(item, "_")
 
+    req = requests.get(url=cat_href, headers=headers)
+    src = req.text
+    with open() as file:
+        file.write(src)
+
+    count+=
